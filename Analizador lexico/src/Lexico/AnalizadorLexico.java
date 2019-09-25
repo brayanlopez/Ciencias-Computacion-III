4d11e7bd86d6c5002d979596b8cafef3d1763d04
brayanlopez <balg.lg55@gmail.com>
2019-09-25 08:00:09 -0500

    Añadio clase Jframe

---
+++ Analizador lexico/src/Lexico/AnalizadorLexico.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lexico;
+
+import java.io.File;
+
+/**
+ *
+ * @author estudiantes
+ */
+public class AnalizadorLexico {
+
+    public static void main(String[] args) {
+        String camino = "direccion archivo flex";
+        generarLex(camino);
+    }
+
+    public static void generarLex(String camino) {
+        File arc = new File(camino);
+        JFlex.Main.generate(arc);
+    }
+}
